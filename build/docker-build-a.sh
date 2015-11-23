f83b76c0cb60b30537b8f9b020b3a7bb27471bab
Gus Segura <kyanyoga@yahoo.com>
2015-11-23 15:21:15 -0600

    added file changes

--- build/docker-build-a.sh
+++ build/docker-build-a.sh
@@ -9,7 +9,7 @@ done
 # for i in {0..10}; do
     # build the containers
 	#docker run --name splunk-fwd-$i -d my_fwd 
-	# docker exec splunk-fwd-$i entrypoint.sh splunk add forward-server 172.17.137.10:9997 -auth admin:changeme
+	# docker exec splunk-fwd-$i entrypoint.sh splunk add forward-server 10.10.10.10:9997 -auth admin:changeme
 	# docker exec splunk-fwd-$i entrypoint.sh splunk add monitor /opt/perfmon/sample_data -index docker_sandbox
 	# docker exec	-d splunk-fwd-$i /bin/bash /opt/perfmon/bin/auto
     # final piece is auto start script
